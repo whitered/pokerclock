4ed6c1f057c0a3dc64c99827e294d7fd98ee4c2b
whitered <dmitry.zhelnin@gmail.com>
2011-12-14 17:25:28 +0400

    initial commit: empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pokerclock::Application.config.secret_token = 'de9ac375630778bb5f295c60d0b098a6cc8dd53877eb0c580dc3eaf25cc0a870988ee4a06515c72f337b72a39925315eeb31e484dffe4d9c150a02d8f63a5e72'
