6e59b8d782b1f0e664150de650546ea7c9681296
whitered <dmitry.zhelnin@gmail.com>
2011-12-21 15:31:39 +0400

    i18n-js locales fixed, some compiled stuff removed from repo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Pokerclock::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( clock.js clock.css )
+  config.assets.precompile += %w( clock.js clock.css locales/*.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
