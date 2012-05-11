32e20ba3a0bcc396ddd3f9791da09290607b2ac9
whitered <dmitry.zhelnin@gmail.com>
2012-05-11 22:53:58 +0400

    pick secret token from the env variables

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Pokerclock::Application.config.secret_token = 'de9ac375630778bb5f295c60d0b098a6cc8dd53877eb0c580dc3eaf25cc0a870988ee4a06515c72f337b72a39925315eeb31e484dffe4d9c150a02d8f63a5e72'
+Pokerclock::Application.config.secret_token = ENV['SECRET_TOKEN']
